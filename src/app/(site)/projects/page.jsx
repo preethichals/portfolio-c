f3e97ac04957397c583fb62653d9b7ea5ec8080c
preethichals <108888412+preethichals@users.noreply.github.com>
2023-04-27 18:33:41 +0530

    first commit

---
+++ src/app/(site)/projects/page.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+import { PortableText } from "@portabletext/react";
+import { getProjects } from "../../../../sanity/sanity_utils";
+
+export default async function ProjectPage() {
+  const projects = await getProjects();
+  return (
+    <>
+      <div className=" bg-white m-4" id="projectlist">
+        <h4 className="text-5xl font-extrabold text-center m-2 headingcursive bg-gradient-to-r from-orange-400 via-red-500 to-cyan-700 bg-clip-text text-transparent>Projects">
+          {" "}
+         ----------- Projects -----------
+        </h4>
+        <div
+          className="grid justify-items-center grid-cols-1 
+        md:gap-x-5 gap-y-4 sm:grid-cols-2 md:grid-cols-3
+        sm:gap-4 lg:gap-5 bg-gray-200 p-4 border-2 border-gray-300 rounded-2xl"
+        >
+          {projects.map((product) => (
+            <div key={product._id} className="rounded-lg m-2">
+              <h5 className="font-medium text-gray-900 text-xl p-2">
+                {product.name}
+              </h5>
+              {product.image && (
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  width={400}
+                  height={300}
+                  className="object-contain shadow-xl border-b-4 border-gray-400"
+                />
+              )}
+
+              <h5 className="m-2 text-base text-gray-700">
+                <PortableText value={product.content} />
+              </h5>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
